74beaa3309d3324809e9287d230db5afe603bbe1
Syed Sayem <syed@hotmail.com>
2013-09-29 11:27:20 -0400

    Added script6 example

---
+++ exercise1/script6.js
@@ -0,0 +1,12 @@
+var names = ["Jeremy", "Jeffrey"],
+    names2 = ["Jennifer", "Jackie"];
+    
+var people = names.concat(names2);
+
+var joined = people.join(", ");
+
+var reversed = people.reverse();
+
+var sorted = people.sort();
+
+alert(sorted);
